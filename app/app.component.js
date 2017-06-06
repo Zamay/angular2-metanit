102e4781e51c9db89e40eedf0ae89d7ac3991306
zaamy <mityazamay@gmail.com>
2017-06-06 16:18:07 +0300

    ngIf

--- app/app.component.js
+++ app/app.component.js
@@ -8,14 +8,44 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
+        this.condition = true;
     }
+    AppComponent.prototype.toggle = function () {
+        this.condition = !this.condition;
+    };
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: "<div>\n\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0<p bold [selectedSize]=\"'28px'\" [defaultSize]=\"'14px'\">Hello Angular 2</p>\n\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0<p>Angular 2 \u043F\u0440\u0435\u0434\u0441\u0442\u0430\u0432\u043B\u044F\u0435\u0442 \u043C\u043E\u0434\u0443\u043B\u044C\u043D\u0443\u044E \u0430\u0440\u0445\u0438\u0442\u0435\u043A\u0442\u0443\u0440\u0443 \u043F\u0440\u0438\u043B\u043E\u0436\u0435\u043D\u0438\u044F</p>\n\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0</div>"
+        template: "<p *ngIf=\"condition;else unset\">\n\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u041F\u0440\u0438\u0432\u0435\u0442 \u043C\u0438\u0440\n\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0</p>\n\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0<ng-template #unset>\u00A0 \n\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0<p>\u041F\u043E\u043A\u0430 \u043C\u0438\u0440</p>\u00A0 \n\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0</ng-template>\u00A0\u00A0\n\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0<button (click)=\"toggle()\">Toggle</button>"
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
+//
+// template: ` <div *ngIf="condition; then thenBlock else elseBlock"></div>  
+//             <ng-template #thenBlock>Then template</ng-template> 
+//             <ng-template #elseBlock>Else template</ng-template>   
+//             <button (click)="toggle()">Toggle</button>`
+/////////////////////////////////////////////////////////////////////////////////////
+// import { Component} from '@angular/core';
+//
+// @Component({
+//     selector: 'my-app',
+//     template: `<p *ngIf="condition">
+//                   Привет мир
+//                 </p>
+//                 <p *ngIf="!condition">
+//                   Пока мир
+//                 </p>
+//                 <button (click)="toggle()">Toggle</button>`
+// })
+// export class AppComponent {
+//
+//     condition: boolean=true;
+//
+//     toggle(){
+//         this.condition=!this.condition;
+//     }
+// } 
 //# sourceMappingURL=app.component.js.map
--- app/app.component.ts
+++ app/app.component.ts
@@ -2,9 +2,48 @@ import { Component} from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<div>
-                  <p bold [selectedSize]="'28px'" [defaultSize]="'14px'">Hello Angular 2</p>
-                  <p>Angular 2 представляет модульную архитектуру приложения</p>
-               </div>`
+    template: `<p *ngIf="condition;else unset">
+                  Привет мир
+                </p>
+                <ng-template #unset>  
+                  <p>Пока мир</p>  
+                </ng-template>  
+                <button (click)="toggle()">Toggle</button>`
 })
-export class AppComponent {}
+export class AppComponent {
+
+    condition: boolean=true;
+
+    toggle(){
+        this.condition=!this.condition;
+    }
+}
+
+//
+// template: ` <div *ngIf="condition; then thenBlock else elseBlock"></div>  
+//             <ng-template #thenBlock>Then template</ng-template> 
+//             <ng-template #elseBlock>Else template</ng-template>   
+//             <button (click)="toggle()">Toggle</button>`
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+// import { Component} from '@angular/core';
+//
+// @Component({
+//     selector: 'my-app',
+//     template: `<p *ngIf="condition">
+//                   Привет мир
+//                 </p>
+//                 <p *ngIf="!condition">
+//                   Пока мир
+//                 </p>
+//                 <button (click)="toggle()">Toggle</button>`
+// })
+// export class AppComponent {
+//
+//     condition: boolean=true;
+//
+//     toggle(){
+//         this.condition=!this.condition;
+//     }
+// }

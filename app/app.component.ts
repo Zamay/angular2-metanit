90d806cd9a40a7042dc0f14130b0938ce519ae4f
zaamy <mityazamay@gmail.com>
2017-06-07 17:19:09 +0300

    Состояние модели и валидация

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,74 +1,43 @@
 import { Component} from '@angular/core';
-import { NgModel} from '@angular/forms';
 
-export class Phone{
-    constructor(public title: string,
-                public price: number,
-                public company: string)
-    { }
+
+export class User{
+    name: string;
+    email: string;
+    phone: string;
 }
 
 @Component({
     selector: 'my-app',
-    template: `<div class="col-xs-10"> 
-<!--                    <div class="form-group">-->
-<!--                        <label>Название модели</label>-->
-<!--                        <input class="form-control" name="title"-->
-<!--                            [(ngModel)]="phone.title" #phoneTitle="ngModel" />-->
-<!--                    </div>-->
-<!--/////////////////////////////-->
-                    <!--<input class="form-control" name="title"-->
-                        <!--[(ngModel)]="phone.title" #phoneTitle="ngModel"-->
-                        <!--(change)="onTitleChange()" />-->
-<!--/////////////////////////////-->
-                    <input class="form-control" name="title"
-                        [(ngModel)]="phone.title" #phoneTitle="ngModel"
-                        (ngModelChange)="onTitleChange()" />
+    styles: [`
+        input.ng-touched.ng-invalid {border:solid red 2px;}
+        input.ng-touched.ng-valid {border:solid green 2px;}
+    `],
+    template: `<div> 
                     <div class="form-group">
-                        <label>Цена</label>
-                        <input type="number" class="form-control" name="price"
-                            [(ngModel)]="phone.price" #phonePrice="ngModel" />
+                        <label>Имя</label>
+                        <input class="form-control" name="name" [(ngModel)]="user.name" #name="ngModel" required />
                     </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input class="form-control" type="email" name="email" [(ngModel)]="user.email" #email="ngModel"
+                            required email />
+                    </div>
                     <div class="form-group">
-                        <label>Производитель</label>
-                        <select class="form-control" name="company"
-                            [(ngModel)]="phone.company" #phoneCompany="ngModel">
-                            <option  *ngFor="let comp of companies" [value]="comp">
-                                {{comp}}
-                            </option>
-                        </select>
+                        <label>Телефон</label>
+                        <input class="form-control" name="phone" [(ngModel)]="user.phone" #phone="ngModel" 
+                            required pattern="[0-9]{10}" />
                     </div>
                     <div class="form-group">
-                        <button class="btn btn-default" (click)="addPhone(phoneTitle, phonePrice, phoneCompany)">
-                            Добавить
-                        </button>
+                        <button [disabled]="name.invalid || email.invalid || phone.invalid"
+                                class="btn btn-default" (click)="addUser()">Добавить</button>
                     </div>
-                    <div>
-                        <p>{{phoneTitle.name}} : {{phoneTitle.model}}</p>
-                        <p>{{phonePrice.name}} : {{phonePrice.model}}</p>
-                        <p>{{phoneCompany.name}} : {{phoneCompany.model}}</p>
-                    </div>
-              </div>
-                <div><h3>Добавленные элементы</h3>
-                    <ul *ngFor="let p of phones">
-                        <li>{{p.title}} ({{p.company}}) - {{p.price}}</li>
-                    </ul>
-                </div>
-              `
+              </div>`
 })
 export class AppComponent {
 
-    phones: Phone[] = [];
-    phone: Phone = new Phone("", 0, "Samsung");
-    companies: string[] = ["Apple", "Huawei", "Xiaomi", "Samsung", "LG", "Motorola", "Alcatel"];
-
-    addPhone(){
-        this.phones.push(new Phone(this.phone.title, this.phone.price, this.phone.company));
-    }
-
-    onTitleChange(){
-
-        if(this.phone.title=="нет")
-            this.phone.title = "не известно";
+    user: User = new User();
+    addUser(){
+        console.log(this.user);
     }
 }
